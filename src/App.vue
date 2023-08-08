07cdfcc3e25f236be1c797384a041df968dc3c5e
InawoVictor <inawovictor004@gmail.com>
2023-08-08 14:08:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col font-Roboto bg-weather-primary min-h-screen">
+    <site-navigation/>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+  import {RouterView} from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input 
+        type="text" placeholder="Search for a City or State"
+        class="py-2 px-1 w-full bg-transparent border-b
+        focus:border-weather-secondary focus:outline-none
+        focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+    
+  </main>
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+
+  const searchQuery = ref("")
+
+</script>
+
